59b1b6fbc2d148b04b65c0a26862122badcc097f
Maddie <madelen.sletteberg@gmail.com>
2024-05-07 10:58:27 +0200

    working on making the register form functional

---
+++ js/handlers/register.mjs
@@ -0,0 +1,10 @@
+export function initiateRegisterFormListener() {
+    const form = document.querySelector("#register-form"); 
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const formData = new FormData(form);
+        const account = Object.fromEntries(formData.entries());
+});
+}
---
+++ js/pages/register.mjs
@@ -0,0 +1,6 @@
+import { hamburgerMenuInit } from "../utils/hamburgerMenu.mjs";
+import { initiateRegisterFormListener } from "../handlers/register.mjs";
+
+hamburgerMenuInit();
+
+initiateRegisterFormListener(); 
