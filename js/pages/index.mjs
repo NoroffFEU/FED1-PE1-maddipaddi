041ca3d8396ce4f275ff60281fef311784e52279
maddipaddi <madelen.sletteberg@gmail.com>
2024-05-14 13:52:46 +0200

    created functions to render post details on blogfeed

--- js/pages/index.mjs
+++ js/pages/index.mjs
@@ -1,8 +1,9 @@
 import { hamburgerMenuInit } from "../utils/hamburgerMenu.mjs";
 import { getPosts } from "../api/posts/read.mjs";
-import { removePost } from "../api/posts/delete.mjs";
+import { renderPosts } from "../templates/post.mjs";
 
 hamburgerMenuInit();
 
 getPosts();
 
+renderPosts();
---
+++ js/templates/post.mjs
@@ -0,0 +1,37 @@
+import { getPosts } from "../api/posts/read.mjs";
+
+export function postTemplate(postData) {
+    const title = document.createElement("h3");
+    title.innerText = postData.title;
+    const date = document.createElement("p");
+    date.innerText = postData.created;
+    const author = document.createElement("p");
+    author.innerText = postData.author.name;
+    const media = document.createElement("img");
+    media.setAttribute("src", postData.media.url);
+    const body = document.createElement("p");
+    body.innerText = postData.body;
+    const tags = document.createElement("p");
+    tags.innerText = postData.tags;
+
+    const post = document.createElement("div");
+    post.classList.add("post");
+    post.append(title, date, author, media, body, tags);
+    return post;
+}
+
+
+export function renderPostsTemplate(posts){
+    const blogFeed = document.querySelector(".blogfeed");
+    posts.forEach(post => {
+       
+       blogFeed.appendChild(postTemplate(post)); 
+    });
+}
+
+
+export async function renderPosts() {
+      const postList = await getPosts();
+      const posts = postList.data;
+      renderPostsTemplate(posts);
+}
