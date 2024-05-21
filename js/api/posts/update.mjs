bb1c774c7d22c7ed12552c8708ba74daf94ffca3
maddipaddi <madelen.sletteberg@gmail.com>
2024-05-21 15:34:01 +0200

    added alerts for better ux

--- js/api/posts/update.mjs
+++ js/api/posts/update.mjs
@@ -1,21 +1,27 @@
-import { API_BASE_URL } from "../constants.mjs";
+import * as API from "../constants.mjs";
 import { fetchToken } from "../auth/fetchToken.mjs";
 
-const action = "/posts"; 
-const method = "put";
+const API_ENDPOINT = API.API_ENDPOINT_BLOG_POSTS + API.API_ENDPOINT_NAME;
+const API_BASE_URL = API.API_BASE_URL;
+const method = "PUT";
 
 export async function updatePost(postData){
-
     if (!postData.id) {
-        throw new Error("Update requires a postID");
+        throw new Error("Update requires a post ID");
     }
 
-    const updatePostURL = `${API_BASE_URL}${action}/${postData.id}`; 
+    const updatePostURL = `${API_BASE_URL}${API_ENDPOINT}/${postData.id}`; 
     
     const response = await fetchToken(updatePostURL, {
-        method, 
+        method,
         body: JSON.stringify(postData)
     })
+
+    if (response.ok) {
+        alert("Post updated successfully!");
+    } else {
+        alert("Failed to update the post");
+    }
 
     return await response.json();
 }
