43dbc8dcdf97f6dfe222afb8f60930c5c11ca57e
Maddie <madelen.sletteberg@gmail.com>
2024-05-25 21:25:25 +0200

    Minor styling and cleanup
    Removed page link, added active class

--- js/utils/loginNav.mjs
+++ js/utils/loginNav.mjs
@@ -5,6 +5,18 @@ export function loginNav() {
     const createNewPost = document.createElement("li");
     const createNewPostLink = document.createElement("a");
     createNewPostLink.setAttribute("href", "../../post/create.html");
+
+    
+    function linkActive() {
+        const pathName = window.location.pathname;
+        if (pathName.endsWith("/post/create.html")){
+            createNewPostLink.classList.add("active");
+        } else {
+        createNewPostLink.classList.remove("active");
+        }
+    }
+    linkActive(); 
+
     createNewPostLink.innerText = "Create new post";
 
     createNewPost.appendChild(createNewPostLink);
