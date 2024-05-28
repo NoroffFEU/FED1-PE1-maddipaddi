03dd54b9840e1f9d2a62292e2ae95a55af4e1b39
maddipaddi <madelen.sletteberg@gmail.com>
2024-05-28 14:22:13 +0200

    still on this path bullshit

--- js/utils/pathFix.mjs
+++ js/utils/pathFix.mjs
@@ -1,5 +1,5 @@
 export function fixPath() {
 const checkForHome = !(window.location.pathname.includes("post")|| window.location.pathname.includes("account"));
-const prePath = checkForHome? "" :"../";
+const prePath = checkForHome? "" :"../../";
 return prePath;
 }
