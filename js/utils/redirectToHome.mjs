a25f77eaf25d0db442d50b1e07a610cfaa38ab85
maddipaddi <madelen.sletteberg@gmail.com>
2024-05-28 22:17:07 +0200

    pathing still going strong

--- js/utils/redirectToHome.mjs
+++ js/utils/redirectToHome.mjs
@@ -1,5 +1,5 @@
 export function redirectToHome() {
     const basePath = window.location.pathname.split("/").slice(0, -2).join("/");
-    const baseUrl = window.location.origin + basePath;
-    window.location.href = baseUrl + "/index.html";
+    const baseUrl = window.location.origin + basePath + "/";
+    window.location.href = baseUrl + "index.html"; 
 }
