5bb42611c56802d441f47c2191995d37207cc2a4
maddipaddi <madelen.sletteberg@gmail.com>
2024-05-28 22:21:13 +0200

    pathinggg

--- js/utils/redirectToHome.mjs
+++ js/utils/redirectToHome.mjs
@@ -1,5 +1,6 @@
 export function redirectToHome() {
-    const basePath = window.location.pathname.split("/").slice(0, -2).join("/");
+    const pathArray = window.location.pathname.split("/");
+    const basePath = pathArray.slice(0, -2).join("/");
     const baseUrl = window.location.origin + basePath + "/";
     window.location.href = baseUrl + "index.html"; 
 }
